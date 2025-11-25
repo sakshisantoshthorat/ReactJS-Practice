b564330745575e7cffe3ba2841d93234c0a34221
Sakshi Thorat <159508795+sakshisantoshthorat@users.noreply.github.com>
2025-11-25 04:49:55 -0500

    ThemeContext.js

--- DarkLightTheme/ThemeContext.js
+++ DarkLightTheme/ThemeContext.js
@@ -1,1 +1,25 @@
 
+import React, { createContext, useEffect, useState } from 'react'
+import './App.css'
+
+export const ThemeContext = createContext();
+export function ThemeProvider({ children }) {
+    const [theme, setTheme] = useState(
+        localStorage.getItem("theme") || "light"
+    );
+
+    useEffect(() => {
+        localStorage.setItem("theme", theme);
+        document.body.className = theme;
+    }, [theme])
+
+    const toggleTheme = () => {
+        setTheme(prev => (prev === "light" ? "dark" : "light"));
+    };
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
